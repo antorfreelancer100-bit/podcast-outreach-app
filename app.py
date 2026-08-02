b5e60fc1c45d3e5434554fb82575e0352938d108
antorfreelancer100-bit <antorfreelancer100@gmail.com>
2026-08-02 07:26:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if st.button("🚀 Research & Write Email"):
 
             try:
                 response = client.models.generate_content(
-                    models/gemini-1.5-flash,
+                    model="gemini-2.0-flash",
                     contents=user_prompt,
                     config=types.GenerateContentConfig(
                         system_instruction=system_instruction,
